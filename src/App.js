1e696b64acd668f9c633ca1a6f520814db217662
nilSinger <niilz@outlook.de>
2020-11-20 20:58:27 +0100

    First commit! Serializing an image (base64) and sending it to the backend works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.jpg";
+import "./App.css";
+import React, { useState, useRef, useEffect } from "react";
 
 function App() {
+  let [data, setData] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas onData={(imgData) => setData(imgData)} />
+      <button onClick={() => totodoist(data)}>Send image Data</button>
     </div>
   );
+}
+
+function Canvas(props) {
+  const canvas = useRef();
+  const image = useRef();
+
+  const draw = () => {
+    let width = canvas.current.width;
+    let height = canvas.current.height;
+    let ctx = canvas.current.getContext("2d");
+    ctx.drawImage(image.current, 0, 0, width, height);
+    //let imageData = ctx.getImageData(0, 0, width, height);
+    canvas.current.toBlob((blob) => props.onData(blob), "image/jpeg");
+  };
+  return (
+    <>
+      <canvas width="500px" height="500px" ref={canvas} />
+      <img
+        ref={image}
+        src={logo}
+        onLoad={draw}
+        className="App-logo"
+        alt="logo"
+      />
+    </>
+  );
+}
+
+function totodoist(imgData) {
+  let r = new FileReader();
+  r.readAsDataURL(imgData);
+  r.onloadend = () => {
+    let imgData64 = r.result.replace("data:image/jpeg;base64,", "");
+    console.log(imgData64);
+    fetch("http://localhost:8080/to-do", {
+      method: "POST",
+      body: imgData64,
+    }).then(console.log);
+  };
 }
 
 export default App;
