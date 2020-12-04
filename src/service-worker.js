ac3c041085e5073ab40765124b8ddc064a345e6e
nilSinger <niilz@outlook.de>
2020-12-04 09:13:42 +0100

    Removing wasm-into-sw-integration attempts

--- src/service-worker.js
+++ src/service-worker.js
@@ -16,29 +16,33 @@ let contentToCache = [
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install");
-  e.waitUntil(
-    caches.open(caches).then((cache) => {
-      console.log("[Service Worker] Caching all: app shell and content");
-      return cache.addAll(contentToCache);
-    })
-  );
+  e.waitUntil(precache());
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      return (
-        r ||
-        fetch(e.request).then(async (response) => {
-          let cache = await caches.open(cacheName);
-          console.log(
-            `[Service Worker] Caching new resource: ${e.request.url}`
-          );
-          cache.put(e.request, response.clone());
-          return resposne;
-        })
-      );
-    })
-  );
+  e.respondWith(fromCache(e.request).then((response) => response));
+  e.waitUntil(update(e.request).then((_unused) => _unused));
 });
+
+async function precache() {
+  let cache = await caches.open(cacheName);
+  console.log("[Service Worker] Caching all: app shell and content");
+  return cache.addAll(contentToCache);
+}
+
+async function update(request) {
+  let cache = await caches.open(cacheName);
+  let response = await fetch(request);
+  console.log(`[Service Worker] Updated request ${request} with ${response}`);
+  cache.put(request, response);
+}
+
+async function fromCache(request) {
+  let cache = await caches.open(cacheName);
+  let matching = await cache.match(request);
+  let logMsg = matching
+    ? `[Service Worker] Serving cached resource ${request.url}`
+    : `[Service Worker] Could not retrieve resource ${request.url}`;
+  console.log(logMsg);
+  return matching || Promise.reject("no-match");
+}
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,23 @@
+// import init, { todoist_from_handwriting } from "./pkg/pen_to_todoist";
+import * as Todoist from "pen-to-todoist";
+
 export function totodoist(imgData) {
   let r = new FileReader();
   r.readAsDataURL(imgData);
   r.onloadend = () => {
     let imgData64 = r.result.replace("data:image/jpeg;base64,", "");
+    /*
+    fetch("./pen_to_todoist.wasm")
+      .then((response) => response.arrayBuffer())
+      .then((bytes) => WebAssembly.instantiate(bytes, {}))
+      .then((result) =>
+        result.instance.exports.todoist_from_handwriting(imgData64)
+      );
     import("pen-to-todoist").then((module) => {
       let digitalList = module.todoist_from_handwriting(imgData64);
       console.log(digitalList);
     });
+    */
+    Todoist.todoist_from_handwriting(imgData64);
   };
 }
