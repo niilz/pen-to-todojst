81f4b9ba44df60d8a1048e1ca4223125e229d6fc
nilSinger <niilz@outlook.de>
2020-12-07 12:56:00 +0100

    Loads wasm data and can run wasm code. No SW yet

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,15 @@
+import init, { todoist_from_handwriting } from "./pkg/pen_to_todoist.js";
+
 export function totodoist(imgData) {
   let r = new FileReader();
   r.readAsDataURL(imgData);
   r.onloadend = () => {
     let imgData64 = r.result.replace("data:image/jpeg;base64,", "");
+    init().then((_) => todoist_from_handwriting(imgData64));
+    /*
     import("pen-to-todoist").then((module) =>
       module.todoist_from_handwriting(imgData64)
     );
+    */
   };
 }
--- utils.js
+++ utils.js
@@ -1,10 +1,17 @@
+import init, { todoist_from_handwriting } from "./pkg/pen_to_todoist.js";
+
 export function totodoist(imgData) {
   var r = new FileReader();
   r.readAsDataURL(imgData);
   r.onloadend = function () {
     var imgData64 = r.result.replace("data:image/jpeg;base64,", "");
-    import("pen-to-todoist").then(function (module) {
-      return module.todoist_from_handwriting(imgData64);
+    init().then(function (_) {
+      return todoist_from_handwriting(imgData64);
     });
+    /*
+    import("pen-to-todoist").then((module) =>
+      module.todoist_from_handwriting(imgData64)
+    );
+    */
   };
 }
