7744257b10f51ba0fd673176350c51731c268c95
nilSinger <niilz@outlook.de>
2021-01-02 21:01:28 +0100

    babel output changed

--- babel/sw-register.js
+++ babel/sw-register.js
@@ -1,8 +1,6 @@
+/* eslint-disable no-restricted-globals */
 export function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(() => console.log("registered SW"));
+    navigator.serviceWorker.register("/service-worker.js").then(() => console.log("registered SW"));
   }
 }
-
