add512d4a473793bc2d327c8b83b1a91994c47fb
nilSinger <niilz@outlook.de>
2021-01-07 20:49:07 +0100

    Added option to add a new Shopping List if so desired

--- babel/components/ProjectList.js
+++ babel/components/ProjectList.js
@@ -1,11 +1,19 @@
 function ProjectList(props) {
+  const projects = assembleList(props.projects);
   return /*#__PURE__*/React.createElement("div", {
     className: "project-list"
-  }, props.projects.map(p => /*#__PURE__*/React.createElement("div", {
-    onClick: () => props.onConfirm(p.id),
-    key: p.id,
+  }, projects.map(proj => /*#__PURE__*/React.createElement("div", {
+    onClick: () => props.onConfirm(proj.id),
+    key: proj.id,
     className: "project"
-  }, /*#__PURE__*/React.createElement("p", null, p.name))));
+  }, /*#__PURE__*/React.createElement("p", null, proj.name))));
 }
 
 export default ProjectList;
+
+function assembleList(projects) {
+  return [...projects, {
+    id: 0,
+    name: "New Shopping List"
+  }];
+}
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,16 +1,21 @@
 function ProjectList(props) {
+  const projects = assembleList(props.projects);
   return (
     <div className="project-list">
-      {props.projects.map((p) => (
+      {projects.map((proj) => (
         <div
-          onClick={() => props.onConfirm(p.id)}
-          key={p.id}
+          onClick={() => props.onConfirm(proj.id)}
+          key={proj.id}
           className="project"
         >
-          <p>{p.name}</p>
+          <p>{proj.name}</p>
         </div>
       ))}
     </div>
   );
 }
 export default ProjectList;
+
+function assembleList(projects) {
+  return [...projects, { id: 0, name: "New Shopping List" }];
+}
