100b2c7d159eac13f9ac0de70b72d62262db8245
nilSinger <niilz@outlook.de>
2021-01-09 17:44:43 +0100

    Recompiled with with babel when service-worker is enabled

--- babel/index.js
+++ babel/index.js
@@ -1,4 +1,5 @@
 // import "./index.css";
 import App from "./App.js";
 import { registerServiceWorker } from "./sw-register.js";
-ReactDOM.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(App, null)), document.getElementById("root")); // registerServiceWorker();
+ReactDOM.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(App, null)), document.getElementById("root"));
+registerServiceWorker();
